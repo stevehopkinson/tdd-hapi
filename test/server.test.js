5093cc5e128c2dff41eaa9541007f22d78cdc736
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-11-17 16:44:08 +0000

    Add test to check whether the server starts

--- test/server.test.js
+++ test/server.test.js
@@ -3,6 +3,12 @@ const tape = require('tape');
 
 tape('Does the server start?', (t) => {
   server.start(err => {
-    t.error(err);
+    if (err) {
+      t.error(err);
+    } else {
+      t.pass('Server is running');
+    }
+    server.stop();
+    t.end();
   });
 })

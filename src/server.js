0c8bf59bff5a9def3d64e16cb1a3bd99c41da9aa
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-11-17 16:15:41 +0000

    Add basic server code

---
+++ src/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server - new Hapi.Server();
+server.connection({ post: 3000 });
+
+server.start( err =>
+  if (err) throw err;
+  console.log(`Server running at: ${server.info.uri}`);
+)
